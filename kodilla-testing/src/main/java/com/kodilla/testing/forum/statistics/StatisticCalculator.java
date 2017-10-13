007ac56d326aff20b68eee4de668805211a21bea
cur1osity <marcin.gajc@gmail.com>
2017-10-13 09:16:50 +0200

    Solution v.1.0

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticCalculator.java
@@ -0,0 +1,64 @@
+package com.kodilla.testing.forum.statistics;
+
+
+public class StatisticCalculator {
+
+    private double userCount;
+    private double postCount;
+    private double commentCount;
+    private double postCountAvg;
+    private double commentCountAvg;
+    private double commentOnPostAvg;
+    private Statistics statistics;
+
+
+    public double getUserCount() {
+        return userCount;
+    }
+
+    public double getPostCount() {
+        return postCount;
+    }
+
+    public double getCommentCount() {
+        return commentCount;
+    }
+
+    public double getPostCountAvg() {
+        return postCountAvg;
+    }
+
+    public double getCommentCountAvg() {
+        return commentCountAvg;
+    }
+
+    public double getCommentOnPostAvg() {
+        return commentOnPostAvg;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics){
+
+    commentCount = statistics.commentsCount();
+    userCount = statistics.usersNames().size();
+    postCount = statistics.postsCount();
+    postCountAvg = postCount/userCount;
+    commentCountAvg = commentCount/userCount;
+    commentOnPostAvg = postCount/commentCount;
+
+    }
+
+//    public void showStatistics(){
+//
+//        StatisticCalculator statisticCalculator = new StatisticCalculator();
+//        statisticCalculator.calculateAdvStatistics(Statistics statistics);
+//
+//    }
+
+
+
+
+
+
+
+
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -0,0 +1,67 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StatisticsTestSuite {
+
+    @Test
+    public void calculateAdvStatisticsTest() {
+
+        Statistics statisticsMock = mock(Statistics.class);
+
+        StatisticCalculator statisticCalculator = new StatisticCalculator();
+
+        // gdy liczba postów = 0
+
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+        Assert.assertEquals(0, statisticCalculator.getPostCount(), 0);
+
+        // gdy liczba postów = 1000
+
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+        Assert.assertEquals(1000, statisticCalculator.getPostCount(), 0);
+
+        // gdy liczba komentarzy = 0
+
+        Assert.assertEquals(0, statisticCalculator.getCommentCount(),0);
+
+        // gdy liczba użytkowników = 0
+
+        Assert.assertEquals(0,statisticCalculator.getUserCount(),0);
+
+        // gdy liczba komentarzy > liczba postów
+
+        when(statisticsMock.postsCount()).thenReturn(5);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+        Assert.assertTrue(statisticCalculator.getPostCount() < statisticCalculator.getCommentCount());
+
+        // gdy liczba komentarzy < liczba postów
+
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(5);
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+        Assert.assertTrue(statisticCalculator.getPostCount() > statisticCalculator.getCommentCount());
+
+        // gdy liczba użytkowników = 100
+
+        List<String> expectedList = new ArrayList<>();
+
+        for(int i = 0; i < 100; i++){
+            expectedList.add("name");
+        }
+
+        when(statisticsMock.usersNames()).thenReturn(expectedList);
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+        Assert.assertEquals(100, statisticCalculator.getUserCount(),0);
+
+    }
+}
