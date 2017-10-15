48912a93b6b19992c913ec1c1648be137c811c9b
cur1osity <marcin.gajc@gmail.com>
2017-10-15 16:07:56 +0200

    Zadanie 6.5 update

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/StatisticsTestSuite.java
@@ -13,57 +13,127 @@ import static org.mockito.Mockito.when;
 
 public class StatisticsTestSuite {
 
-    @Test
-    public void calculateAdvStatisticsTest() {
-
-        Statistics statisticsMock = mock(Statistics.class);
-
-        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
-
-        // gdy liczba postów = 0
-
-        statisticCalculator.calculateAdvStatistics(statisticsMock);
-        Assert.assertEquals(0, statisticCalculator.getPostCount(), 0);
-
-        // gdy liczba postów = 1000
-
-        when(statisticsMock.postsCount()).thenReturn(1000);
-        statisticCalculator.calculateAdvStatistics(statisticsMock);
-        Assert.assertEquals(1000, statisticCalculator.getPostCount(), 0);
-
-        // gdy liczba komentarzy = 0
-
-        Assert.assertEquals(0, statisticCalculator.getCommentCount(),0);
-
-        // gdy liczba użytkowników = 0
-
-        Assert.assertEquals(0,statisticCalculator.getUserCount(),0);
-
-        // gdy liczba komentarzy > liczba postów
-
-        when(statisticsMock.postsCount()).thenReturn(5);
-        when(statisticsMock.commentsCount()).thenReturn(10);
-        statisticCalculator.calculateAdvStatistics(statisticsMock);
-        Assert.assertTrue(statisticCalculator.getPostCount() < statisticCalculator.getCommentCount());
-
-        // gdy liczba komentarzy < liczba postów
-
-        when(statisticsMock.postsCount()).thenReturn(10);
-        when(statisticsMock.commentsCount()).thenReturn(5);
-        statisticCalculator.calculateAdvStatistics(statisticsMock);
-        Assert.assertTrue(statisticCalculator.getPostCount() > statisticCalculator.getCommentCount());
-
-        // gdy liczba użytkowników = 100
+    private List<String> createUsers(int count) {
 
         List<String> expectedList = new ArrayList<>();
 
-        for(int i = 0; i < 100; i++){
+        for (int i = 0; i < count; i++) {
             expectedList.add("name");
         }
+        return expectedList;
+    }
 
-        when(statisticsMock.usersNames()).thenReturn(expectedList);
+
+    @Test
+    public void Should_Have0Comments_When_CommentCountIs0() {
+
+        // Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
+        when(statisticsMock.commentsCount()).thenReturn(0);
+
+        //When
         statisticCalculator.calculateAdvStatistics(statisticsMock);
-        Assert.assertEquals(100, statisticCalculator.getUserCount(),0);
+
+        //Then
+        Assert.assertEquals(0, statisticCalculator.getCommentCount(), 0);
+
+    }
+
+    @Test
+    public void Should_Have0Posts_When_PostCountIs0() {
+
+        // Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
+        when(statisticsMock.postsCount()).thenReturn(0);
+
+        //When
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(0, statisticCalculator.getPostCount(), 0);
+
+    }
+
+    @Test
+    public void Should_Have1000Posts_When_PostCountIs1000() {
+
+        // Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
+        when(statisticsMock.postsCount()).thenReturn(1000);
+
+        //When
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(1000, statisticCalculator.getPostCount(), 0);
+
+    }
+
+    @Test
+    public void Should_BeTrue_When_PostCountIsLowerThanCommentsCount() {
+
+        // Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
+        when(statisticsMock.postsCount()).thenReturn(5);
+        when(statisticsMock.commentsCount()).thenReturn(10);
+
+        //When
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertTrue(statisticCalculator.getPostCount() < statisticCalculator.getCommentCount());
+    }
+
+    @Test
+    public void Should_BeTrue_When_PostCountIsHigherThanCommentsCount() {
+
+        // Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(5);
+
+        //When
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertTrue(statisticCalculator.getPostCount() > statisticCalculator.getCommentCount());
+    }
+
+    @Test
+    public void Should_Have0Users_When_UserCountIs0() {
+
+        // Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
+        when(statisticsMock.usersNames()).thenReturn(createUsers(0));
+
+        //When
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(0, statisticCalculator.getUserCount(), 0);
+
+    }
+
+
+    @Test
+    public void Should_Have100Users_When_UserCountIs100() {
+
+        // Given
+        Statistics statisticsMock = mock(Statistics.class);
+        StatisticsCalculator statisticCalculator = new StatisticsCalculator();
+        when(statisticsMock.usersNames()).thenReturn(createUsers(100));
+
+        //When
+        statisticCalculator.calculateAdvStatistics(statisticsMock);
+
+        //Then
+        Assert.assertEquals(100, statisticCalculator.getUserCount(), 0);
 
     }
 
