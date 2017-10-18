5eb9ee0bdc6d44f33b40b36e7013384798430c8e
cur1osity <marcin.gajc@gmail.com>
2017-10-18 13:14:11 +0200

    Wstep - modul 7

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/Subtraction.java
@@ -0,0 +1,9 @@
+package com.kodilla.stream.lambda;
+
+public class Subtraction implements MathExpression {
+
+    @Override
+    public double calculateExpression(double a, double b) {
+        return a - b;
+    }
+}
