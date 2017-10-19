5bb14f30890ac586e20a49c9889fa4324645ed0d
cur1osity <marcin.gajc@gmail.com>
2017-10-19 11:28:09 +0200

    Zadanie 7.2 v.1.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,16 @@
 package com.kodilla.stream;
 
-
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.immutable.ForumUser;
+import com.kodilla.stream.person.People;
 
 public class StreamMain {
     public static void main(String[] args) {
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf('N')))
+  //              .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier("Bla bla bla");
-
-        poemBeautifier.beautify(n -> n.toUpperCase());
-        poemBeautifier.beautify(n -> n.toLowerCase());
-        poemBeautifier.beautify(n -> "ABC" + n + "ABC");
-        poemBeautifier.beautify(n -> n.replace('a','x'));
-
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
     }
-
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,20 +1,11 @@
 package com.kodilla.stream.immutable;
 
-public final class ForumUser {
+import lombok.Value;
 
-   private final String userName;
-   private final String realName;
+@Value
+public class ForumUser {
 
-    public ForumUser(final String userName, final String realName) {
-        this.userName = userName;
-        this.realName = realName;
-    }
+   private String userName;
+   private String realName;
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
 }
