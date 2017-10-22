a451a82279334504497d9cabac1ddb254c3e0be4
cur1osity <marcin.gajc@gmail.com>
2017-10-22 14:14:10 +0200

    Zadanie 7.5 v.1.1

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -9,10 +9,14 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
 
+        // Given
         int[] array = {1, 4, 23, 100, 45, 12, 18, 32, 54, 120, 33, 187, 300, 12, 55, 106, 200, 5, 55, 12};
 
+        // When
+        double average = ArrayOperations.getAverage(array);
 
-        Assert.assertEquals(68.7, ArrayOperations.getAverage(array),0.001);
+        // Then
+        Assert.assertEquals(68.7, average,0.001);
 
     }
 }
