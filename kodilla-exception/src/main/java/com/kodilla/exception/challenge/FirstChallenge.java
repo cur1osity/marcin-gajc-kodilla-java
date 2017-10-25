c9ac20fc11365a42bd252f30ec28d488a4940748
cur1osity <marcin.gajc@gmail.com>
2017-10-25 12:04:04 +0200

    Zadnie 8.1 v.1.0

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
@@ -0,0 +1,35 @@
+package com.kodilla.exception.challenge;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+
+        try {
+            if (b == 0) {
+                throw new ArithmeticException();
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Can't divde by 0: " + e);
+
+        } finally {
+
+            return a / b;
+        }
+
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+
+    }
+}
