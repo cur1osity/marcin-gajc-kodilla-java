3cc4cf4ab0017fab2670d1be9975856ee9cb32bd
cur1osity <marcin.gajc@gmail.com>
2017-10-25 14:11:23 +0200

    Zadanie 8.2 v.1.0

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,24 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(5,2);
+
+        } catch (Exception e) {
+
+            System.out.println("Error");;
+
+        } finally {
+
+            System.out.println("Done");
+        }
+
+    }
+
+
+}
