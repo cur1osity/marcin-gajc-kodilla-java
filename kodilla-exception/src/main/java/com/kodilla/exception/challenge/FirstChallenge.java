9b3ccddf05cfe55e2498031d28ca0604d48f48d6
cur1osity <marcin.gajc@gmail.com>
2017-10-27 08:16:37 +0200

    Zadanie 8.1 v.3.0

--- kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/challenge/FirstChallenge.java
@@ -3,22 +3,10 @@ package com.kodilla.exception.challenge;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-        } catch (ArithmeticException e) {
-            System.out.println("Can't divde by 0: " + e);
-
-        } finally {
-
-            if(b == 0){
-                return a/b;
-            }
-
-            return a/b;
-
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
     }
+
 }
--- kodilla-exception/src/test/java/com/kodilla/exception/challenge/FirstChallengeTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/challenge/FirstChallengeTestSuite.java
@@ -8,24 +8,32 @@ import org.junit.Test;
 public class FirstChallengeTestSuite {
 
 
+    @Test(expected = ArithmeticException.class)
+    public void Should_throw_exception_When_divide_by_0() {
+
+        //given
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        // ???
+        try {
+            firstChallenge.divide(1,0);
+        } catch (ArithmeticException e) {
+            System.out.println("Can't divide by 0");
+        }
+    }
 
     @Test
-    public void Should_caught_exception_When_divide_by_0() {
+    public void Should_not_throw_exception_When_divide_by_other_than_0() {
 
         //given
         FirstChallenge firstChallenge = new FirstChallenge();
 
         //when
-        String exception ="";
-
-        try {
-            firstChallenge.divide(1, 0);
-        } catch (ArithmeticException e) {
-            exception = e.toString();
-        }
+        double result = firstChallenge.divide(2,4);
 
         //then
-        Assert.assertEquals("",exception);
+        Assert.assertEquals(0.5,result,0.001);
 
     }
 }
+
