f0780fe73b8d07ae5ad9137785d2946c733bf11b
cur1osity <marcin.gajc@gmail.com>
2017-11-02 16:24:58 +0100

    Zadanie 9.2 v.1.1

--- kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/challenges/OrdersTestSuite.java
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/challenges/OrdersTestSuite.java
@@ -1,22 +1,42 @@
 package com.kodilla.good.patterns.challenges;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class OrdersTestSuite {
+
+    private OrderRequestRetriever orderRequestRetriever;
+    private OrderRequest orderRequest;
+    private ProductRepository productRepository;
+    private ProductOrderService productOrderService;
+    private OrderProcessor orderProcessor;
+
+
+    @Before
+    public void setup() {
+
+        orderRequestRetriever = new OrderRequestRetriever();
+        orderRequest = orderRequestRetriever.retrieve();
+        productRepository = new ProductRepository();
+        productOrderService = new ProductOrderService(true);
+        orderProcessor = new OrderProcessor(new MailService(), productOrderService, productRepository);
+    }
+
+    @After
+    public void stars() {
+        System.out.println("********************************************");
+    }
 
     @Test
     public void Should_Have_1_Element_List_Size_When_Order_is_Correct() {
 
         //given
-        OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
-        OrderRequest orderRequest = orderRequestRetriever.retrieve();
-        ProductRepository productRepository = new ProductRepository();
-        ProductOrderService productOrderService = new ProductOrderService(true);
-        OrderProcessor orderProcessor = new OrderProcessor(new MailService(), productOrderService, productRepository);
-        orderProcessor.process(orderRequest);
+        // @Before
 
         //when
+        orderProcessor.process(orderRequest);
         int numberOfOrders = productRepository.getOrders().size();
 
         //then
@@ -28,14 +48,13 @@ public class OrdersTestSuite {
     public void Should_Have_0_Element_List_Size_When_Order_is_not_Correct() {
 
         //given
-        OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
-        OrderRequest orderRequest = orderRequestRetriever.retrieve();
-        ProductRepository productRepository = new ProductRepository();
+        //@Before +
         ProductOrderService productOrderService = new ProductOrderService(false);
-        OrderProcessor orderProcessor = new OrderProcessor(new MailService(), productOrderService, productRepository);
-        orderProcessor.process(orderRequest);
+        orderProcessor = new OrderProcessor(new MailService(), productOrderService, productRepository);
+
 
         //when
+        orderProcessor.process(orderRequest);
         int numberOfOrders = productRepository.getOrders().size();
 
         //then
@@ -48,14 +67,11 @@ public class OrdersTestSuite {
     public void Should_Have_Dto_with_parameter_True_When_Order_is_correct() {
 
         //given
-        OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
-        OrderRequest orderRequest = orderRequestRetriever.retrieve();
-        ProductRepository productRepository = new ProductRepository();
-        ProductOrderService productOrderService = new ProductOrderService(true);
-        OrderProcessor orderProcessor = new OrderProcessor(new MailService(), productOrderService, productRepository);
-        ProductDto dto = orderProcessor.process(orderRequest);
+        //@Before
+
 
         //when
+        ProductDto dto = orderProcessor.process(orderRequest);
         boolean isTrue = dto.isOrdered();
 
         //then
@@ -67,14 +83,12 @@ public class OrdersTestSuite {
     public void Should_Have_Dto_with_parameter_False_When_Order_is_not_correct() {
 
         //given
-        OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
-        OrderRequest orderRequest = orderRequestRetriever.retrieve();
-        ProductRepository productRepository = new ProductRepository();
+        //@Before +
         ProductOrderService productOrderService = new ProductOrderService(false);
-        OrderProcessor orderProcessor = new OrderProcessor(new MailService(), productOrderService, productRepository);
-        ProductDto dto = orderProcessor.process(orderRequest);
+        orderProcessor = new OrderProcessor(new MailService(), productOrderService, productRepository);
 
         //when
+        ProductDto dto = orderProcessor.process(orderRequest);
         boolean isFalse = dto.isOrdered();
 
         //then
