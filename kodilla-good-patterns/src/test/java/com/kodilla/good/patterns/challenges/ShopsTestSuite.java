8082770fc962e3ba53a1703b3e5969737573480b
cur1osity <marcin.gajc@gmail.com>
2017-11-03 09:43:46 +0100

    Zadanie 9.3 v.1.2

--- kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/challenges/ShopsTestSuite.java
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/challenges/ShopsTestSuite.java
@@ -1,34 +1,41 @@
 package com.kodilla.good.patterns.challenges;
 
+import com.kodilla.good.patterns.challenge1.*;
 import com.kodilla.good.patterns.challenge1.OrderRequest;
-import com.kodilla.good.patterns.challenge1.OrderRequestRetriever;
+import com.kodilla.good.patterns.challenge1.Product;
 import com.kodilla.good.patterns.challenge1.ProductDto;
-import com.kodilla.good.patterns.challenge1.ShopProcessor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ShopsTestSuite {
 
-    private OrderRequestRetriever orderRequestRetriever;
     private OrderRequest orderRequest;
     private ShopProcessor shopProcessor;
+    private Shop shop;
 
+
+    public OrderRequest retrieve(){
+
+        Product product = new Product("eggs");
+
+        return new OrderRequest(product, 10, shop);
+
+    }
 
     @Before
-    public void setup(){
+    public void setup() {
 
-        orderRequestRetriever = new OrderRequestRetriever();
-        orderRequest = orderRequestRetriever.retrieve();
         shopProcessor = new ShopProcessor();
     }
 
 
     @Test
-    public void Should_return_right_shop_name_When_order_is_correct() {
+    public void Should_return_ExtraFoodShop_name_When_order_is_correct() {
 
         //given
-        // @Before + orderRequestRetriver
+        shop = new ExtraFoodShop();
+        orderRequest = retrieve();
 
         //when
         ProductDto productDto = shopProcessor.generate(orderRequest);
@@ -36,6 +43,38 @@ public class ShopsTestSuite {
 
         //given
         Assert.assertEquals("ExtraFoodShop",shopName);
+
+    }
+
+    @Test
+    public void Should_return_GlutenFreeShop_name_When_order_is_correct() {
+
+        //given
+        shop = new GlutenFreeShop();
+        orderRequest = retrieve();
+
+        //when
+        ProductDto productDto = shopProcessor.generate(orderRequest);
+        String shopName = productDto.getShop().getClass().getSimpleName();
+
+        //given
+        Assert.assertEquals("GlutenFreeShop",shopName);
+
+    }
+
+    @Test
+    public void Should_return_HealthyShop_name_When_order_is_correct() {
+
+        //given
+        shop = new HealthyShop();
+        orderRequest = retrieve();
+
+        //when
+        ProductDto productDto = shopProcessor.generate(orderRequest);
+        String shopName = productDto.getShop().getClass().getSimpleName();
+
+        //given
+        Assert.assertEquals("HealthyShop",shopName);
 
     }
 }
