ad240f983f8c36c6186ecf9d9d5e03e6b57e350f
cur1osity <marcin.gajc@gmail.com>
2017-11-16 15:41:03 +0100

    Zadanie 11.3 v.1.0

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.patterns.strategy.social;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+
+    @Test
+    public void Should_return_proper_publisher_name_When_sharePost_is_used() {
+
+        //given
+        User a = new Millenials("FacebookUser");
+        User b = new YGeneration("TwitterUser");
+        User c = new ZGeneration("SnapchatUser");
+
+        //when
+        String aShouldUse = a.sharePost();
+        String bShouldUse = b.sharePost();
+        String cShouldUse = c.sharePost();
+
+        //then
+        Assert.assertEquals("Facebook", aShouldUse);
+        Assert.assertEquals("Twitter", bShouldUse);
+        Assert.assertEquals("Snapchat", cShouldUse);
+    }
+
+    @Test
+    public void Should_change_publisher_strategy_When_setSocialPublisherStrategy_is_used() {
+
+        //given
+        User a = new Millenials("FacbookUser");
+
+        //when
+        a.setSocialPublisherStrategy(new SnapchatPublisher());
+        String aShouldUse = a.sharePost();
+
+        //then
+        Assert.assertEquals("Snapchat", aShouldUse);
+    }
+}
