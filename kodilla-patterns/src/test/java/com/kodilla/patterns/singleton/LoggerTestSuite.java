411d8122fb10e1cd5a403b18af60c1522c8b0877
cur1osity <marcin.gajc@gmail.com>
2017-11-16 13:45:04 +0100

    Zadanie 11.1 v.1.0

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,1 +1,26 @@
+package com.kodilla.patterns.singleton;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+    @Test
+    public void test1() {
+
+        //given
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("myLog");
+        logger2.log("mySecondLog");
+
+        // when
+        String log1 = logger1.getLastLog();
+
+
+        //then
+        Assert.assertEquals("mySecondLog", log1);
+
+    }
+}
