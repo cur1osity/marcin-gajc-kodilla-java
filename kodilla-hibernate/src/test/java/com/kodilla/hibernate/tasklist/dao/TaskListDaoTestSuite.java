1c22f2d5eee8330cbbf368f8a0d59478f40507f4
cur1osity <marcin.gajc@gmail.com>
2017-12-04 18:36:12 +0100

    Modul 13 c.d.

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,5 +1,7 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,33 +9,66 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.math.BigDecimal;
 
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaskListDaoTestSuite {
 
+    private static final String LISTNAME = "ToDo list";
+
     @Autowired
     private TaskListDao taskListDao;
-    private static final String DESCRIPTION = "First list";
+    private static final String DESCRIPTION = "ToDo tasks";
+
+//    @Test
+//    public void Should_HaveOneTaskList_When_FindByListNameIsUsed() {
+//
+//        //Given
+//        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+//        taskListDao.save(taskList);
+//        String listName = taskList.getListName();
+//
+//        //When
+//        List<TaskList> theTaskList = taskListDao.findByListName(listName);
+//
+//        //Then
+//        Assert.assertEquals(2, theTaskList.size());
+//
+//        //CleanUp
+//        int id = theTaskList.get(0).getId();
+//        taskListDao.delete(id);
+//    }
 
     @Test
-    public void Should_HaveOneTaskList_When_FindByListNameIsUsed() {
-
+    public void testTaskListDaoSaveWithTasks() {
         //Given
-        TaskList taskList = new TaskList("List1", DESCRIPTION);
-        taskListDao.save(taskList);
-        String listName = taskList.getListName();
+        Task task = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Test: Write some entities", 3);
+
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
 
         //When
-        List<TaskList> theTaskList = taskListDao.findByListName(listName);
+        taskListDao.save(taskList);
+        int id = taskList.getId();
 
         //Then
-        Assert.assertEquals(1, theTaskList.size());
+        Assert.assertNotEquals(0, id);
 
         //CleanUp
-        int id = theTaskList.get(0).getId();
         taskListDao.delete(id);
     }
 }
