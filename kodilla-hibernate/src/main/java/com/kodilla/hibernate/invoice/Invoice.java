2cd54bc134aba930574f2f9ce95120aa74fc48c8
cur1osity <marcin.gajc@gmail.com>
2018-01-18 11:23:50 +0100

    kodilla-hibernate3 -> kodilla-hibernate

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -0,0 +1,45 @@
+package com.kodilla.hibernate.invoice;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "INVOICES")
+@Data
+@NoArgsConstructor
+public class Invoice {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    private int id;
+    @Column(name="INVOICE_NUMBER")
+    private String number;
+
+    @OneToMany (
+            targetEntity = Item.class,
+            mappedBy = "invoice",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+
+    )
+    private List<Item> items = new ArrayList<>();
+
+    public Invoice(String number) {
+        this.number = number;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,40 @@
+package com.kodilla.hibernate.tasklist;
+
+import com.kodilla.hibernate.task.Task;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TASKLIST")
+@Data
+@NoArgsConstructor
+public final class TaskList {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    private int id;
+    @Column(name="LISTNAME")
+    private String listName;
+    @Column(name = "DESCRIPTION")
+    private String description;
+    @OneToMany (
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Task> tasks = new ArrayList<>();
+
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+}
