e96e122311d6497ee8bc1bc895794e84657858b7
cur1osity <marcin.gajc@gmail.com>
2018-01-23 14:01:30 +0100

    Modul 20 c.d.

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/company/SalaryAdaptee.java
@@ -0,0 +1,14 @@
+package com.kodilla.patterns2.adapter.company;
+
+import com.kodilla.patterns2.adapter.company.newhrsystem.CompanySalaryProcessor;
+import com.kodilla.patterns2.adapter.company.newhrsystem.Employee;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SalaryAdaptee {
+    public BigDecimal calculateSalaries(List<Employee> employees){
+        CompanySalaryProcessor theProcessor = new CompanySalaryProcessor();
+        return theProcessor.calculateSalaries(employees);
+    }
+}
