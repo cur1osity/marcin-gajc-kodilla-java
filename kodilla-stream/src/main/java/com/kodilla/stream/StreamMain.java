e0771855d050b1d1855ef462091f8c795f315f55
cur1osity <marcin.gajc@gmail.com>
2018-02-09 17:41:41 +0100

    Modul 22

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,16 +11,25 @@ import java.util.stream.Collectors;
 public class StreamMain {
     public static void main(String[] args) {
 
-        Forum forum = new Forum();
+//        Forum forum = new Forum();
+//
+//        Map<Integer, ForumUser> theMapOfUsers = forum.getList().stream()
+//                .filter(user -> user.getGender() == ForumUser.Gender.M)
+//                .filter(user -> user.getBirthDate().getYear() < 1997)
+//                .filter(user -> user.getPostCount() > 1)
+//                .collect(Collectors.toMap(ForumUser::getId, user -> user));
+//
+//        theMapOfUsers.entrySet().stream()
+//                .map(entry -> (entry.getKey() + ": " + entry.getValue()))
+//                .forEach(System.out::println);
 
-        Map<Integer, ForumUser> theMapOfUsers = forum.getList().stream()
-                .filter(user -> user.getGender() == ForumUser.Gender.M)
-                .filter(user -> user.getBirthDate().getYear() < 1997)
-                .filter(user -> user.getPostCount() > 1)
-                .collect(Collectors.toMap(ForumUser::getId, user -> user));
 
-        theMapOfUsers.entrySet().stream()
-                .map(entry -> (entry.getKey() + ": " + entry.getValue()))
-                .forEach(System.out::println);
+//        System.out.println("Using Stream to generate even numbers from 1 to 20");
+//        NumbersGenerator.generateEven(20);
+
+        NumbersGenerator numbersGenerator = new NumbersGenerator();
+
+        numbersGenerator.generateEven(20);
+
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -3,7 +3,7 @@ package com.kodilla.stream.iterate;
 import java.util.stream.*;
 
 public final class NumbersGenerator {
-    public static void generateEven(int max) {
+    public void generateEven(int max) {
         Stream.iterate(1, n -> n + 1)
                 .limit(max)
                 .filter(n -> n % 2 != 0)
